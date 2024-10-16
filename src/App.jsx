b665d2c19833f7d978a1cde1488b08df9a6a7984
Hinoshii_38 <thapatw.bs@gmail.com>
2024-10-16 23:39:35 +0700

    a

---
+++ src/App.jsx
@@ -0,0 +1,110 @@
+import React, { useState, useEffect, useRef } from "react";
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-backend-webgl"; // set backend to webgl
+import Loader from "./components/loader";
+import ButtonHandler from "./components/btn-handler";
+import { detect, detectVideo } from "./utils/detect";
+import "./style/App.css";
+
+const App = () => {
+  const [loading, setLoading] = useState({ loading: true, progress: 0 }); // loading state
+  const [model, setModel] = useState({
+    net: null,
+    inputShape: [1, 0, 0, 3],
+  }); // init model & input shape
+  const [modelName, setModelName] = useState("cyolov8n");
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  // references
+  const imageRef = useRef(null);
+  const cameraRef = useRef(null);
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const loadModel = async () => {
+      setLoading({ loading: true, progress: 0 });
+  
+      tf.ready().then(async () => {
+        const yolov8 = await tf.loadGraphModel(
+          `${window.location.href}/${modelName}_web_model/model.json`,
+          {
+            onProgress: (fractions) => {
+              setLoading({ loading: true, progress: fractions }); // set loading fractions
+            },
+          }
+        ); // load model
+  
+        // warming up model
+        const dummyInput = tf.ones(yolov8.inputs[0].shape);
+        const warmupResults = yolov8.execute(dummyInput);
+  
+        setLoading({ loading: false, progress: 1 });
+        setModel({
+          net: yolov8,
+          inputShape: yolov8.inputs[0].shape,
+        }); // set model & input shape
+  
+        tf.dispose([warmupResults, dummyInput]); // cleanup memory
+      });
+    };
+  
+    loadModel();
+
+    // const ctx = canvasRef.current.getContext("2d");
+    // ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+
+  }, [modelName]);
+
+  const handleModelChange = (event) => {
+    setModelName(event.target.value); // Update the selected model
+    setRefreshKey((prevKey) => prevKey + 1);
+  };
+
+  return (
+    <div className="App" key={refreshKey}>
+      {loading.loading && <Loader>Loading model... {(loading.progress * 100).toFixed(2)}%</Loader>}
+      <div className="header">
+        <h1>📷 YOLO Live Detection App</h1>
+        <p>
+          YOLO live detection application on browser powered by <code>tensorflow.js</code>
+        </p>
+        <p>
+          Serving : <code className="code">{modelName}</code>
+        </p>
+      </div>
+
+      {/* Dropdown to change model */}
+      <label htmlFor="model-select">Choose Model: </label>
+        <select id="model-select" value={modelName} onChange={handleModelChange}>
+          <option value="cyolov9t">YOLOv9 Tiny</option>
+          <option value="cyolov8n">YOLOv8 Nano</option>
+        </select>
+
+      <div className="content">
+        <img
+          src="#"
+          ref={imageRef}
+          onLoad={() => detect(imageRef.current, model, canvasRef.current)}
+        />
+        <video
+          autoPlay
+          muted
+          ref={cameraRef}
+          onPlay={() => detectVideo(cameraRef.current, model, canvasRef.current)}
+        />
+        <video
+          autoPlay
+          muted
+          ref={videoRef}
+          onPlay={() => detectVideo(videoRef.current, model, canvasRef.current)}
+        />
+        <canvas width={model.inputShape[1]} height={model.inputShape[2]} ref={canvasRef} />
+      </div>
+
+      <ButtonHandler imageRef={imageRef} cameraRef={cameraRef} videoRef={videoRef}/>
+    </div>
+  );
+};
+
+export default App;
