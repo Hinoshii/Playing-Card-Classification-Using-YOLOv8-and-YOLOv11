6cdb73740e892cbeb85140dbfa72b4bc9d99281e
kitti-best <kittibest46@gmail.com>
2024-10-17 23:22:08 +0700

    suicide

--- README.md
+++
@@ -1,5 +0,0 @@
-Clone this repo and type "yarn install" and "yarn start"
-## Reference
-
-- https://github.com/ultralytics/ultralytics
-- https://github.com/Hyuto/yolov8-onnxruntime-web
--- Train Model/train_yolov11.py
+++
@@ -1,9 +0,0 @@
-import ultralytics
-from ultralytics import YOLO
-
-def main():
-    model = YOLO("yolo11n.pt")
-    results = model.train(data="yameaw.yaml", epochs=10, imgsz=640)
-
-if __name__ == '__main__':
-    main()
